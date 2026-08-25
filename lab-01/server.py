3cdbe895d4bf9406842e096a642b270ac06884c3
leticialopes <l184423@dac.unicamp.br>
2026-08-25 20:38:01 -0300

    trying to implement threads

--- lab-01/server.py
+++ lab-01/server.py
@@ -3,19 +3,13 @@ from socket import *
 import threading
 import sys  # In order to terminate the program
 
-serverPort = 12000
+serverPort = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepare a sever socket
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 
-def handle_thread():
-    pass
-
-while True:
-    # Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr =  serverSocket.accept()
+def handle_thread(connectionSocket):
     try:
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
@@ -27,14 +21,23 @@ while True:
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
     except IOError:
         # Send response message for file not found
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-        # Close client socket
-        connectionSocket.close()
-        serverSocket.close()
-
     connectionSocket.close()
-    serverSocket.close()
-    sys.exit()  # Terminate the program after sending the corresponding data
+
+def main():
+    while True:
+        # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr =  serverSocket.accept()
+        client_thread = threading.Thread(
+            target=handle_thread,
+            args=(connectionSocket,)
+        )
+        client_thread.start()
+        client_thread.join()
+        # sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+    main()
