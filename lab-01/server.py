669e34599778b3aa61a1e52d79e19c7be7e12fb4
Letícia Lopes <l184423@dac.unicamp.br>
2026-08-26 15:49:29 -0300

    server is now working with threads

--- lab-01/server.py
+++ lab-01/server.py
@@ -3,13 +3,12 @@ from socket import *
 import threading
 import sys  # In order to terminate the program
 
-serverPort = 12001
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
+def send_data(connectionSocket, outputdata):
+    for i in range(0, len(outputdata)):
+        connectionSocket.send(outputdata[i].encode())
+    connectionSocket.send("\r\n".encode())
 
-def handle_thread(connectionSocket):
+def handle_client(connectionSocket):
     try:
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
@@ -18,26 +17,37 @@ def handle_thread(connectionSocket):
         # Send one HTTP header line into socket
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        send_data(connectionSocket, outputdata)
     except IOError:
         # Send response message for file not found
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        f = open('404.html')
+        outputdata = f.read()
+        send_data(connectionSocket, outputdata)
+    # Close client socket
     connectionSocket.close()
 
 def main():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a sever socket
+    serverPort = 12000
+    serverSocket.bind(('',serverPort))
+    serverSocket.listen(1)
+
     while True:
         # Establish the connection
         print('Ready to serve...')
         connectionSocket, addr =  serverSocket.accept()
-        client_thread = threading.Thread(
-            target=handle_thread,
-            args=(connectionSocket,)
-        )
-        client_thread.start()
-        client_thread.join()
-        # sys.exit()  # Terminate the program after sending the corresponding data
+        try:
+            # Create a thread to handle the client connection
+            client_thread = threading.Thread(target=handle_client, args=(connectionSocket,))
+            client_thread.start()
+        except KeyboardInterrupt:
+            serverSocket.close()
+            sys.exit()  # Terminate the program
+
+    serverSocket.close()
+    sys.exit()  # Terminate the program
 
 if __name__ == "__main__":
     main()
