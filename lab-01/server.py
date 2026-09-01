2a0ad10538ee4be9aa2e65e6ff8968520d6e3c14
Letícia Lopes <l184423@dac.unicamp.br>
2026-09-01 19:21:19 -0300

    updating server

--- lab-01/server.py
+++ lab-01/server.py
@@ -3,36 +3,33 @@ from socket import *
 import threading
 import sys  # In order to terminate the program
 
-def send_data(connectionSocket, outputdata):
-    for i in range(0, len(outputdata)):
-        connectionSocket.send(outputdata[i].encode())
-    connectionSocket.send("\r\n".encode())
-
 def handle_client(connectionSocket):
     try:
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata =  f.read() # read file
+        outputdata =  f.read() # Read file
         # Send one HTTP header line into socket
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         # Send the content of the requested file to the client
-        send_data(connectionSocket, outputdata)
+        connectionSocket.sendall(outputdata.encode())
     except IOError:
         # Send response message for file not found
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         f = open('404.html')
         outputdata = f.read()
-        send_data(connectionSocket, outputdata)
+        connectionSocket.sendall(outputdata.encode())
     # Close client socket
     connectionSocket.close()
 
 def main():
-    serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a sever socket
+    serverSocket = socket(AF_INET, SOCK_STREAM) # Create a TCP socket
+    serverAddress = ''
     serverPort = 12000
-    serverSocket.bind(('',serverPort))
-    serverSocket.listen(1)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # Allow reuse of the address
+    serverSocket.bind((serverAddress, serverPort)) # Bind the socket to the server address and server port
+    serverSocket.listen(1) # Listen for incoming connections
 
     while True:
         # Establish the connection
@@ -40,7 +37,10 @@ def main():
         connectionSocket, addr =  serverSocket.accept()
         try:
             # Create a thread to handle the client connection
-            client_thread = threading.Thread(target=handle_client, args=(connectionSocket,))
+            client_thread = threading.Thread(
+                target=handle_client,
+                args=(connectionSocket,)
+            )
             client_thread.start()
         except KeyboardInterrupt:
             serverSocket.close()
